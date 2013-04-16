859ced2bdea9e13bb60c6315e79d40438f3309aa
pulliam <pulliam@epi-c02g90q1dj.ad.ufl.edu>
2013-04-16 15:25:38 -0400

    Prep for HW assignment

---
+++ homework.R
@@ -0,0 +1,13 @@
+# homework.R
+# Created: 16 April 2013 by JRCP
+#
+
+# INSTRUCTIONS
+# 1. read a file that has two rows: column names, column data types
+#
+# 2. based on the input from that file, read another binary file that has those columns with those data types
+#
+# ADDITIONAL INFO
+# - The first file will be ascii text (strictly speaking, the english letter/number character subset of utf-8),
+# no specific mimetype (aka, extension). single space between columns, newline between rows 
+# - The second file will be binary data, no specific mimetype.
